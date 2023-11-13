ebb3a454aa8471dd99126c5911de440eb67b9702
Quicory <bill_software@hotmail.com>
2023-11-13 19:43:37 -0400

    Commit Inicial

--- school/Controllers/UserController.cs
+++ school/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using School_Data.DTOs;
 using System.Data;
 
 namespace School_API.Controllers
@@ -11,7 +12,7 @@ namespace School_API.Controllers
     public class UserController : ControllerBase
     {
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<APIResponse> Get()
         {
             var uerlist = await Task.FromResult(new string[] { "Virat", "Messi", "Ozil", "Lara", "MS Dhoni" });
             return Ok(uerlist);
