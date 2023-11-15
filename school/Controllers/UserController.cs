2879a0e1e720584ded0cbeb94be7a3a90edc2e17
Quicory <bill_software@hotmail.com>
2023-11-15 11:04:38 -0400

    Actualizar datos

--- school/Controllers/UserController.cs
+++ school/Controllers/UserController.cs
@@ -11,11 +11,21 @@ namespace School_API.Controllers
     [Authorize(Roles = "Admin")]
     public class UserController : ControllerBase
     {
+        private readonly ILogger<UserController> _logger;
+        protected APIResponse _resp;
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+            _resp = new();
+        }
+
         [HttpGet]
         public async Task<APIResponse> Get()
         {
             var uerlist = await Task.FromResult(new string[] { "Virat", "Messi", "Ozil", "Lara", "MS Dhoni" });
-            return Ok(uerlist);
+            _resp.Result= uerlist;
+            _resp.StatusCode = System.Net.HttpStatusCode.OK;
+            return _resp;
         }
     }
 }
---
+++ school_data/DTOs/PagingDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_Data.DTOs
+{
+    public class PagingDTO
+    {
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public string? FieldOrder { get; set; }
+        public bool IsAsc { get; set; } = true;
+        public string? Filter { get; set; }
+    }
+}
