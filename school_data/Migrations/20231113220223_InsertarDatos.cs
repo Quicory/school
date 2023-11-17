a9fde652d51fea85f5134670adaad23f3a5fff82
Quicory <bill_software@hotmail.com>
2023-11-17 12:06:21 -0400

    Reestablecer datos anteriores

---
+++ school_data/Migrations/20231113220223_InsertarDatos.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace School_Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertarDatos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "ad376a8f-9eab-4bb9-9fca-30b01540f445", null, "Admin", "Admin" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "CompleteName", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "a18be9c0-aa65-4af8-bd17-00bd9344e575", 0, "admin", "59f7574e-3684-477a-bab7-4deec9332aed", "admin@gmail.com", false, false, null, "admin@gmail.com", "Admin", "AQAAAAIAAYagAAAAEIM29uLZK19LVgOpRpiMyKsMP8ynKZJ7/1Dj5IADtSFfaCZuSdQ6pVw3LSDnFt3dRA==", null, false, "", false, "Admin" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "ad376a8f-9eab-4bb9-9fca-30b01540f445", "a18be9c0-aa65-4af8-bd17-00bd9344e575" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "ad376a8f-9eab-4bb9-9fca-30b01540f445", "a18be9c0-aa65-4af8-bd17-00bd9344e575" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ad376a8f-9eab-4bb9-9fca-30b01540f445");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e575");
+        }
+    }
+}
